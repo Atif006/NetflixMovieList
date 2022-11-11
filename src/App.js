33aaa78624bbe1cf1d2970cc5cfd5e3761a47a61
irfanmd06 <mdirfanraj0786@gmail.com>
2022-11-12 00:29:47 +0530

    change in css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import "./components/netflixStyle.css";
+import Container from "./components/Container";
+// import Card from "./components/Card";
+import axios from "axios";
+const API_KEY = "a9118a3a";
 
 function App() {
+  const [searchQuery, updateSearchquery] = useState();
+  const [movieList, updateMovieList] = useState([]);
+  const [timeoutId, updateTimeOutId] = useState();
+  const fetchData = async (searchString) => {
+    const response = await axios.get(
+      `https://www.omdbapi.com/?s=${searchString}&apikey=${API_KEY}`
+    );
+    updateMovieList(response.data.Search);
+  };
+
+  const searchMovie = (e) => {
+    clearTimeout(timeoutId);
+
+    updateSearchquery(e.target.value);
+    const timeout = setTimeout(() => fetchData(searchQuery), 500);
+    updateTimeOutId(timeout);
+    // fetchData(e.target.value);
+  };
+  console.log(movieList);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main-container">
+        <Header searchMovie={searchMovie} />
+      </div>
+      <div className="mMargin">
+        <Container movies={movieList} />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import mlogo from "./mylogo.png";
+import SearchIcon from "@mui/icons-material/Search";
+
+const Header = (props) => {
+  return (
+    <div>
+      <div className="header">
+        <div className="Left">
+          <img src={mlogo} altlogo />
+          <span> Movie Lists</span>
+        </div>
+        <div className="right">
+          <SearchIcon className="sicon" />
+          <input type="text" onChange={props.searchMovie} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
